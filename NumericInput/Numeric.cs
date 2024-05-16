574d954fcfed1b1244ef2e3eafe4d7142170c670
maldron0309 <bagjinhyeong640@gmail.com>
2024-05-17 03:18:26 +0900

    Add project files.

---
+++ NumericInput/Numeric.cs
@@ -0,0 +1,45 @@
+﻿namespace NumericInput
+{
+    public class Numeric
+    {
+        public static string ReadInput()
+        {
+            string input = Console.ReadLine();
+
+            return input;
+        }
+
+        public static bool IsValidNum(string input)
+        {
+            double res;
+
+            return double.TryParse(input, out res);
+        }
+
+        public static int ReadInteger()
+        {
+            string input;
+            int value;
+
+            while (true)
+            {
+                input = ReadInput();
+                if (int.TryParse(input, out value)) return value;
+                else Console.WriteLine("Invalid input. Please enter an integer.");
+            }
+        }
+
+        public static double ReadDouble()
+        {
+            string input;
+            double value;
+
+            while (true)
+            {
+                input = ReadInput();
+                if (double.TryParse(input, out value)) return value;
+                else Console.WriteLine("Invalid input. Please enter a number.");
+            }
+        }
+    }
+}
